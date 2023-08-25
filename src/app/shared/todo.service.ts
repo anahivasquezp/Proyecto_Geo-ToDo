7101eda13f7b9ba8cc9d19176acae4dfaa663555
Alexis Vizuete <alexis.vizuete@epn.edu.ec>
2023-08-25 11:51:52 -0500

    Creación tarea con id de usuario y mostrar las tareas solo del id autenticado

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore'
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  firestoreCollection : AngularFirestoreCollection;
+  firestoreCollection: AngularFirestoreCollection;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private afAuth: AngularFireAuth) {
     this.firestoreCollection = firestore.collection('todos');
-   }
+  }
 
-   addTodo(title: string){
-     this.firestoreCollection.add({
-       title,
-       isDone : false
-     })
-   }
+  addTodoWithUserId(title: string, userId: string) {
+    this.firestoreCollection.add({
+      title: title,
+      isDone: false,
+      userId: userId
+    });
+  }
 
-   updateTodoStatus(id:string, newStatus:boolean){
-     this.firestoreCollection.doc(id).update({isDone:newStatus});
-   }
+  addTodo(title: string) {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        const userId = user.uid;
+        this.addTodoWithUserId(title, userId);
+      }
+    });
+  }
 
-   deleteTodo(id:string){
-     this.firestoreCollection.doc(id).delete();
-   }
+  updateTodoStatus(id: string, newStatus: boolean) {
+    this.firestoreCollection.doc(id).update({ isDone: newStatus });
+  }
 
+  deleteTodo(id: string) {
+    this.firestoreCollection.doc(id).delete();
+  }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../shared/todo.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-todo',
@@ -11,20 +13,35 @@ export class TodoComponent implements OnInit {
 
   todos: any[] = [];
 
-  constructor(private todoService: TodoService) { }
+  constructor(
+    private todoService: TodoService,
+    private afAuth: AngularFireAuth
+    ) { }
 
-  ngOnInit(): void {
-    this.todoService.firestoreCollection.valueChanges({ idField: 'id' })
-      .subscribe(item => {
-        this.todos = item.sort((a:any,b:any) => 
-        {return a.isDone -b.isDone } );
-      })
-  }
+    ngOnInit(): void {
+      this.afAuth.authState.subscribe(user => {
+        if (user) {
+          const userId = user.uid;
+          this.todoService.firestoreCollection
+            .valueChanges({ idField: 'id' })
+            .subscribe(items => {
+              this.todos = items
+                .filter(item => item.userId === userId)
+                .sort((a: any, b: any) => a.isDone - b.isDone);
+            });
+        }
+      });
+    }
 
   onClick(titleInput: HTMLInputElement) {
     if (titleInput.value) {
-      this.todoService.addTodo(titleInput.value);
-      titleInput.value = "";
+      this.afAuth.authState.subscribe(user => {
+        if (user) {
+          const userId = user.uid;
+          this.todoService.addTodoWithUserId(titleInput.value, userId);
+          titleInput.value = "";
+        }
+      });
     }
   }
 
