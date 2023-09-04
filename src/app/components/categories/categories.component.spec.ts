321431af00fa819ea4b2bfe9399f0fdc00e19997
Anahi Vasquez EPN <anahi.vasquez@epn.edu.ec>
2023-09-03 20:05:04 -0500

    Tests Success Categories Component

--- src/app/components/categories/categories.component.spec.ts
+++ src/app/components/categories/categories.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CategoriesComponent } from './categories.component';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CategoriesService } from '../../services/categories.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { of } from 'rxjs';
 
 describe('CategoriesComponent', () => {
   let component: CategoriesComponent;
   let fixture: ComponentFixture<CategoriesComponent>;
+  let authServiceMock: any;
+  let categoriesServiceMock: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CategoriesComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    // Mock para AuthService
+    authServiceMock = {
+      getAuthenticatedUserId: jasmine.createSpy().and.returnValue('testUserId'),
+    };
+
+    // Mock para CategoriesService
+    categoriesServiceMock = {
+      addCategory: jasmine.createSpy(),
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [CategoriesComponent],
+      providers: [
+        { provide: AngularFireAuth, useValue: {} },
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: CategoriesService, useValue: categoriesServiceMock },
+      ],
+    });
 
     fixture = TestBed.createComponent(CategoriesComponent);
     component = fixture.componentInstance;
@@ -19,5 +38,38 @@ describe('CategoriesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call addCategory when onClick is called with valid input', () => {
+    const nameInput = document.createElement('input');
+    nameInput.value = 'Test Category';
+    component.selectedColor = 'blue';
+
+    component.onClick(nameInput);
+
+    // Verificar que authService.getAuthenticatedUserId haya sido llamado
+    expect(authServiceMock.getAuthenticatedUserId).toHaveBeenCalled();
+
+    // Verificar que categoriesService.addCategory haya sido llamado con los valores correctos
+    expect(categoriesServiceMock.addCategory).toHaveBeenCalledWith(
+      'testUserId',
+      'Test Category',
+      'blue'
+    );
+  });
+
+  
+  it('should not call addCategory when onClick is called with invalid input', () => {
+    const nameInput = document.createElement('input');
+    nameInput.value = ''; // Nombre de categoría vacío
+    component.selectedColor = 'blue';
+
+    component.onClick(nameInput);
+
+    // Verifica que authService.getAuthenticatedUserId haya sido llamado
+    expect(authServiceMock.getAuthenticatedUserId).toHaveBeenCalled();
+
+    // Verifica que categoriesService.addCategory no haya sido llamado
+    expect(categoriesServiceMock.addCategory).not.toHaveBeenCalled();
   });
 });
